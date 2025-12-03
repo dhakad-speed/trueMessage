91221902d0d5807631389cdcd428645d90f5177b
dhakad-speed <dhakadtryspeed@gmail.com>
2025-12-03 11:52:35 +0530

    auth init

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,14 +1,13 @@
 import Login from "@/src/components/layout/LoginPage/Login";
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
-export default function page() {
-  (async () => {
-    const session = await auth();
-    if (session) {
-      redirect("/dashboard");
-    } else {
-      redirect("/register");
-    }
-  })();
+
+export default async function Page() {
+  const session = await auth();
+
+  if (session) {
+    redirect("/dashboard");
+  }
+
   return <Login />;
 }

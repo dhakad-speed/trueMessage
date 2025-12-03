28fdf91ad52dc2fa5f67684393bc442f99568047
dhakad-speed <dhakadtryspeed@gmail.com>
2025-12-03 11:58:53 +0530

    logger removed

--- auth.ts
+++ auth.ts
@@ -4,7 +4,6 @@ import Credentials from "next-auth/providers/credentials";
 import { ConnectDataBase } from "./src/lib/connection";
 import UserModel from "./src/models/User";
 import { verifyPassword } from "./src/lib/secure";
-import { logger } from "./src/lib/logger";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -71,13 +70,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 
         token.name = typeof user.name === "string" ? user.name : token.name;
       }
-      try {
-        logger?.info(token);
-      } catch (error) {
-        // Fallback to console if logger worker has exited
-        console.log("JWT token:", token);
-      }
-      return token;
     },
     async session({ token, session }) {
       if (token) {
